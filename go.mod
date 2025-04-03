4f2a4fee67e75f0f8a696c0076604d220d65419a
Robert Petz <rpetz@ignitelabs.net>
2025-04-03 00:04:55 -0700

    Removed the requirement for a third party library to use EGL

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.24.1
 require (
 	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71
 	github.com/ignite-laboratories/core v0.2.6
-	github.com/remogatto/egl v0.0.0-20140203134355-fea5ba3496ba
 )
 
 require golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
--- graphics/linux.go
+++ graphics/linux.go
@@ -2,19 +2,25 @@
 
 package graphics
 
+/*
+#cgo LDFLAGS: -lEGL -lGL
+#include <EGL/egl.h>
+#include <GL/gl.h>
+#include <stdlib.h>
+*/
+import "C"
 import (
 	"fmt"
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/ignite-laboratories/core"
 	"github.com/ignite-laboratories/core/std"
 	"github.com/ignite-laboratories/host/window"
-	"github.com/remogatto/egl"
 	"log"
 	"runtime"
 )
 
 func init() {
-	fmt.Println("[host] - Linux - sparking EGL graphics bridge")
+	fmt.Println("[host] - Linux - sparking native EGL graphics bridge")
 }
 
 // SparkRenderableWindow creates a new GL renderable window using EGL.
@@ -30,50 +36,49 @@ func sparkEGLBridge(handle window.Handle, renderer Renderable) {
 	defer runtime.UnlockOSThread()
 
 	// Initialize EGL
-	display := egl.GetDisplay(egl.DEFAULT_DISPLAY)
-	if display == egl.NO_DISPLAY {
-		log.Fatal("Failed to get EGL display")
+	display := C.eglGetDisplay(C.EGL_DEFAULT_DISPLAY)
+	if display == nil {
+		log.Fatalf("Failed to get EGL display: %v", getEGLError())
 	}
-	if !egl.Initialize(display, nil, nil) {
-		log.Fatal("Failed to initialize EGL")
+	if C.eglInitialize(display, nil, nil) == C.EGL_FALSE {
+		log.Fatalf("Failed to initialize EGL: %v", getEGLError())
 	}
-	defer egl.Terminate(display)
+	defer C.eglTerminate(display)
 
 	// Choose an EGL configuration
-	attribs := []int32{
-		egl.RED_SIZE, 8, // 8-bit Red channel
-		egl.GREEN_SIZE, 8, // 8-bit Green channel
-		egl.BLUE_SIZE, 8, // 8-bit Blue channel
-		egl.DEPTH_SIZE, 24, // 24-bit Depth buffer
-		egl.STENCIL_SIZE, 8, // 8-bit Stencil buffer
-		egl.SURFACE_TYPE, egl.WINDOW_BIT, // Rendering surface type: window
-		egl.RENDERABLE_TYPE, egl.OPENGL_BIT, // Enable OpenGL rendering
-		egl.NONE, // End of attribute list
-
+	attribs := []C.EGLint{
+		C.EGL_RED_SIZE, 8,
+		C.EGL_GREEN_SIZE, 8,
+		C.EGL_BLUE_SIZE, 8,
+		C.EGL_DEPTH_SIZE, 24,
+		C.EGL_STENCIL_SIZE, 8,
+		C.EGL_SURFACE_TYPE, C.EGL_WINDOW_BIT,
+		C.EGL_RENDERABLE_TYPE, C.EGL_OPENGL_BIT,
+		C.EGL_NONE,
 	}
-	var config egl.Config
-	var numConfigs int32
-	if !egl.ChooseConfig(display, attribs, &config, 1, &numConfigs) || numConfigs == 0 {
-		log.Fatal("Failed to choose EGL configuration")
+	var config C.EGLConfig
+	var numConfigs C.EGLint
+	if C.eglChooseConfig(display, &attribs[0], &config, 1, &numConfigs) == C.EGL_FALSE || numConfigs == 0 {
+		log.Fatalf("Failed to choose EGL configuration: %v", getEGLError())
 	}
 
-	// Create EGL surface for the X11 window
-	surface := egl.CreateWindowSurface(display, config, egl.NativeWindowType(uintptr(handle.Window.ID)), nil)
-	if surface == egl.NO_SURFACE {
-		log.Fatalf("Failed to create EGL surface: %v", egl.GetError())
+	// Create an EGL surface for the X11 window
+	surface := C.eglCreateWindowSurface(display, config, C.EGLNativeWindowType(uintptr(handle.Window.ID)), nil)
+	if surface == nil {
+		log.Fatalf("Failed to create EGL surface: %v", getEGLError())
 	}
-	defer egl.DestroySurface(display, surface)
+	defer C.eglDestroySurface(display, surface)
 
-	// Create an EGL OpenGL context
-	context := egl.CreateContext(display, config, egl.NO_CONTEXT, nil)
-	if context == egl.NO_CONTEXT {
-		log.Fatalf("Failed to create EGL context: %v", egl.GetError())
+	// Create an EGL context
+	context := C.eglCreateContext(display, config, nil, nil)
+	if context == nil {
+		log.Fatalf("Failed to create EGL context: %v", getEGLError())
 	}
-	defer egl.DestroyContext(display, context)
+	defer C.eglDestroyContext(display, context)
 
-	// Initialize EGL and OpenGL for this window
-	if !egl.MakeCurrent(display, surface, surface, context) {
-		log.Fatalf("Failed to make EGL context current: %v", egl.GetError())
+	// Make the current context
+	if C.eglMakeCurrent(display, surface, surface, context) == C.EGL_FALSE {
+		log.Fatalf("Failed to make EGL context current: %v", getEGLError())
 	}
 
 	// Initialize OpenGL
@@ -84,7 +89,47 @@ func sparkEGLBridge(handle window.Handle, renderer Renderable) {
 	// Start rendering
 	for core.Alive {
 		renderer.Render()
+
 		// Swap the buffers to display the rendered frame
-		egl.SwapBuffers(display, surface)
+		C.eglSwapBuffers(display, surface)
+	}
+}
+
+// getEGLError retrieves the EGL error string.
+func getEGLError() string {
+	errorCode := C.eglGetError()
+	switch errorCode {
+	case C.EGL_SUCCESS:
+		return "EGL_SUCCESS"
+	case C.EGL_NOT_INITIALIZED:
+		return "EGL_NOT_INITIALIZED"
+	case C.EGL_BAD_ACCESS:
+		return "EGL_BAD_ACCESS"
+	case C.EGL_BAD_ALLOC:
+		return "EGL_BAD_ALLOC"
+	case C.EGL_BAD_ATTRIBUTE:
+		return "EGL_BAD_ATTRIBUTE"
+	case C.EGL_BAD_CONTEXT:
+		return "EGL_BAD_CONTEXT"
+	case C.EGL_BAD_CONFIG:
+		return "EGL_BAD_CONFIG"
+	case C.EGL_BAD_CURRENT_SURFACE:
+		return "EGL_BAD_CURRENT_SURFACE"
+	case C.EGL_BAD_DISPLAY:
+		return "EGL_BAD_DISPLAY"
+	case C.EGL_BAD_SURFACE:
+		return "EGL_BAD_SURFACE"
+	case C.EGL_BAD_MATCH:
+		return "EGL_BAD_MATCH"
+	case C.EGL_BAD_PARAMETER:
+		return "EGL_BAD_PARAMETER"
+	case C.EGL_BAD_NATIVE_PIXMAP:
+		return "EGL_BAD_NATIVE_PIXMAP"
+	case C.EGL_BAD_NATIVE_WINDOW:
+		return "EGL_BAD_NATIVE_WINDOW"
+	case C.EGL_CONTEXT_LOST:
+		return "EGL_CONTEXT_LOST"
+	default:
+		return fmt.Sprintf("Unknown EGL error: %x", errorCode)
 	}
 }
