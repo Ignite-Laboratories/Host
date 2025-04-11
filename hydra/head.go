14fe709ce8e2198bbe5bbcb80f60979f0b2b975d
Robert Petz <rpetz@ignitelabs.net>
2025-04-10 21:47:21 -0700

    Update head.go

--- hydra/head.go
+++ hydra/head.go
@@ -1,12 +1,10 @@
 package hydra
 
 import (
-	"fmt"
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/ignite-laboratories/core"
 	"log"
 	"runtime"
-	"strings"
 	"sync"
 	"time"
 )
@@ -31,36 +29,36 @@ func (w *Head) start(initialize func(), action core.Action) {
 	// Create OpenGL context
 	glContext, err := w.Window.GLCreateContext()
 	if err != nil {
-		log.Fatalf("[%v] Failed to create OpenGL context: %v", ModuleName, err)
+		log.Fatalf("[%v] failed to create OpenGL context: %v", ModuleName, err)
 	}
 	defer sdl.GLDeleteContext(glContext)
 
 	// Enable VSync
 	if err := sdl.GLSetSwapInterval(0); err != nil {
-		log.Printf("[%v] Failed to set VSync: %v", ModuleName, err)
+		log.Printf("[%v] failed to set VSync: %v", ModuleName, err)
 	}
 
 	// Initialize OpenGL
 	if err := gl.Init(); err != nil {
-		log.Fatalf("[%v] Failed to initialize OpenGL: %v", ModuleName, err)
+		log.Fatalf("[%v] failed to initialize OpenGL: %v", ModuleName, err)
 	}
 
 	// Get OpenGL version
-	glVersion := gl.GoStr(gl.GetString(gl.VERSION))
-	fmt.Printf("[%v] [%d.%d] initialized with %s\n", ModuleName, w.WindowID, w.ID, glVersion)
-
-	fmt.Println("OpenGL Version:", glVersion)
-
-	// Get and print extensions
-	numExtensions := int32(0)
-	gl.GetIntegerv(gl.NUM_EXTENSIONS, &numExtensions)
-
-	for i := int32(0); i < numExtensions; i++ {
-		extension := gl.GoStr(gl.GetStringi(gl.EXTENSIONS, uint32(i)))
-		if strings.Contains(extension, "geometry") {
-			fmt.Println("Found geometry-related extension:", extension)
-		}
-	}
+	//glVersion := gl.GoStr(gl.GetString(gl.VERSION))
+	//fmt.Printf("[%v] [%d.%d] initialized with %s\n", ModuleName, w.WindowID, w.ID, glVersion)
+	//
+	//fmt.Println("openGL Version:", glVersion)
+	//
+	//// Get and print extensions
+	//numExtensions := int32(0)
+	//gl.GetIntegerv(gl.NUM_EXTENSIONS, &numExtensions)
+	//
+	//for i := int32(0); i < numExtensions; i++ {
+	//	extension := gl.GoStr(gl.GetStringi(gl.EXTENSIONS, uint32(i)))
+	//	if strings.Contains(extension, "geometry") {
+	//		fmt.Println("found geometry-related extension:", extension)
+	//	}
+	//}
 
 	initialize()
 
